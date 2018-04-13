afb1e39cf25e02087b7df575e34a0b6372bbfce0
Maxime Gresse <mgresse@debian.univ-jfc.fr>
2018-04-13 16:47:45 +0200

    Sérialisation des images + modification de la fonction pour calculer la couleur

--- src/Modele.java
+++ src/Modele.java
@@ -26,10 +26,26 @@ public abstract class Modele extends Observable{
 				Image img = ImageIO.read(file);
 				Images img_att=new Images(img);
 				this.images.add(img_att);
+				
 				String dimension = img_att.resolution;
 				if (!this.dico.containsKey(dimension))
 					this.dico.put(dimension, new ArrayList<Images>());
 				this.dico.get(dimension).add(img_att);
+				
+				Color coulmoy = img_att.couleur;
+				if (!this.dico.containsKey(coulmoy))
+					this.dico.put(coulmoy.toString(), new ArrayList<Images>());
+				this.dico.get(coulmoy).add(img_att);
+				
+				String nom = file.getName();
+				int index=0;
+				while(nom.charAt(index)!='.'){
+					index+=1;
+				}
+				String nom2 = nom.substring(0,index);
+				if (!this.dico.containsKey(nom2))
+					this.dico.put(nom2.toString(), new ArrayList<Images>());
+				this.dico.get(nom2).add(img_att);
 				
 				
 			} catch (IOException e) {
