bb5302093f164d53d1d3d14534bc71094a15c430
MofidK <mkrim.imp@gmail.com>
2018-05-04 10:53:48 +0200

    avant dernier version de l'interface

--- src/InterfaceGraphique/Controller.java
+++ src/InterfaceGraphique/Controller.java
@@ -16,15 +16,20 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.stage.FileChooser;
 import javafx.stage.Window;
@@ -33,13 +38,13 @@ import javafx.util.Callback;
 public class Controller extends AnchorPane implements Observer{
 
 	@FXML
-	Button b1;
+	Button b1, b2;
 	@FXML
 	Label label1, label2;
 	@FXML
 	CheckBox cb1, cb2, cb3, cb4;
 	@FXML
-	Tab galerie, vue;
+	Tab galerie, edit;
 	@FXML
 	ChoiceBox<String> chb1; /*liste de choix des couleurs*/
 	@FXML
@@ -50,13 +55,14 @@ public class Controller extends AnchorPane implements Observer{
 	ImageView imgview1, imgview2, imgview3, imgview4, imgview5, imgview6, imgview7, imgview8, imgview9, imgview10, imgview11, imgview12, imgview13, imgview14, imgview15;
 
 	Object rech[] = {"",new ArrayList<String>()};
+	ImageView[] im = {imgview1, imgview2, imgview3, imgview4, imgview5, imgview6, imgview7, imgview8, imgview9, imgview10, imgview11, imgview12, imgview13, imgview14, imgview15};
 
 
-	ArrayList<ImageView> imageview;
+	ArrayList<ImageView> imageview=new ArrayList();
 	ArrayList<Images> images;
 
 
-	public void initialize(){
+	public void initialize() throws IOException{
 
 		ObservableList<String> couleurs = FXCollections.observableArrayList("...","Noir","Bleu","Cyan","Gris", "Vert", "Magenta","Orange","Rose","Rouge","Blanc","Jaune");/*Ajout des elements dans la liste de choix des couleurs*/
 		chb1.setItems(couleurs);
@@ -69,28 +75,70 @@ public class Controller extends AnchorPane implements Observer{
 
 		chb2.getSelectionModel().selectFirst();/*Selectionne par défaut le premier element de la liste*/
 
-		//this.genereimages();
+		this.genereimages();
+		//this.aleat();
 
 	}
-	public void selectedimage(ActionEvent event){/*Créer la fonction qui ouvre une nouvelle image dans un nouvel onglet*/
-		if(vue.isSelected()){
+
+	public void aleat() throws IOException{
+		File source = new File("C:/Users/Mofid Krim/Desktop/L2/Java_Project/projetjava/Photos");
+		// de quoi descendre dans les sous répertoires et ainsi tester le no
+		File[] content = source.listFiles();
+		for(int i=0;i<content.length;i++){
+			String localUrl = content[(int) (Math.random()*(content.length-0))].toURI().toURL().toString();
+			Image image = new Image(localUrl);
+			im[i].setImage(image);
 		}
 	}
+	public void editimage(ActionEvent event){
+			Alert alert = new Alert(Alert.AlertType.INFORMATION);
+		    alert.setTitle("Editeur d'images");
+		    alert.setHeaderText(null);
+		    alert.setContentText("Veuillez sélectionner une image");
+		    alert.showAndWait();
+	}
 
-	/*public void genereimages(){
-        final URL imageURL = getClass().getResource(System.getProperty("user.home")+"/Bureau/JAVA/projetjava/Photos/louvre.jpg");
-        final Image image = new Image(imageURL.toExternalForm());
-        final ImageView imageView = new ImageView(image);
+	public void genereimages(){
+        Image image = new Image("file:Photos/larry.jpg");
+        imgview1.setImage(image);
+        Image image1 = new Image("file:Photos/larry.jpg");
+        imgview2.setImage(image1);
+        Image image2 = new Image("file:Photos/larry.jpg");
+        imgview3.setImage(image2);
+        Image image3 = new Image("file:Photos/larry.jpg");
+        imgview4.setImage(image3);
+        Image image4 = new Image("file:Photos/larry.jpg");
+        imgview5.setImage(image4);
+        Image image5 = new Image("file:Photos/larry.jpg");
+        imgview6.setImage(image5);
+        Image image6 = new Image("file:Photos/larry.jpg");
+        imgview7.setImage(image6);
+        Image image7 = new Image("file:Photos/larry.jpg");
+        imgview8.setImage(image7);
+        Image image8 = new Image("file:Photos/larry.jpg");
+        imgview9.setImage(image8);
+        Image image9 = new Image("file:Photos/larry.jpg");
+        imgview10.setImage(image9);
+        Image image10 = new Image("file:Photos/larry.jpg");
+        imgview11.setImage(image10);
+        Image image11 = new Image("file:Photos/larry.jpg");
+        imgview12.setImage(image11);
+        Image image12 = new Image("file:Photos/larry.jpg");
+        imgview13.setImage(image12);
+        Image image13 = new Image("file:Photos/larry.jpg");
+        imgview14.setImage(image13);
+        Image image14 = new Image("file:Photos/larry.jpg");
+        imgview15.setImage(image14);
 
-		File file = new File("Photos/louvres.jpg");
-		System.out.println(System.getProperty("user.home"));
+		//File file = new File("C:/Users/Mofid Krim/Desktop/L2/Java_Project/projetjava/Photos/louvre.jpg");
 
-		System.out.println(file.exists());
-    }*/
+		//System.out.println(System.getProperty("user.home"));
+
+		//System.out.println(file.exists());
+    }
 
 	public void buttonaction(ActionEvent event){		/*Fonction qui agit sur le bouton ajouter filtres, on recupere les filtres choisis*/
 		ArrayList<String> filtres = new ArrayList<>();
-		//if(b1.onMouseClickedProperty() != null){
 			if(cb1.isSelected()){
 				filtres.add(cb1.getText());
 			}
@@ -118,12 +166,13 @@ public class Controller extends AnchorPane implements Observer{
 		//}
 	}
 
-	/*public void toImageView(){
-		for(Images image : this.images){
-			this.imageview.add(new ImageView().setImage(image)s);
-		}
+	public void toImageView(){
+		/*for(int i=0; i<this.images.size();i++){
+			this.imageview.add(imgview1);
+			this.imageview[i].setImage(images[i]);
+		}*/
 
-	}*/
+	}
 
 	public void barrerecherche(ActionEvent event){ /*Fonction qui permet de recuperer les valeurs rentrés dans la barre de recherche*/
 		//System.out.print(brech.getText());/*probleme réncontré, les elements restent en memorie a chaque recherche effectué*/
@@ -138,7 +187,8 @@ public class Controller extends AnchorPane implements Observer{
 	}
 	@Override
 	public void update(Observable obs, Object obj) {
-		//this.images = ((ArrayList<Images> obj);
+		ArrayList img= new ArrayList();
+		img.add(this.images);
 	}
 
 }
